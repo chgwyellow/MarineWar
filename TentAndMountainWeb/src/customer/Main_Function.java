2bcb7593bb26ae846e5bdf3e64751ee9c91377e5
chgwyellow <dinnis1107@gmail.com>
2023-05-05 18:28:53 +0800

    Tent and Mountain Web

--- TentAndMountainWeb/src/customer/Main_Function.java
+++ TentAndMountainWeb/src/customer/Main_Function.java
@@ -1,15 +1,34 @@
 package customer;
 
 
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * @author chgwyellow
  * @create 2023-05-05 17:28
- * 主要功能執行類別
+ * @主要功能執行類別
  */
+
 public class Main_Function {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-
-
+        UserManagement user = new UserManagement();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("請選擇需要的操作: 註冊 登入 查詢資料 或輸入q離開");
+        String result = sc.nextLine();
+        while (!(result.equals("q"))) {
+            if (result.equals("註冊")) {
+                while (!user.succeed)
+                    user.register();
+                break;
+            } else if (result.equals("登入")) {
+                user.login();
+            } else if (result.equals("查詢資料")) {
+                System.out.println("請輸入帳號: ");
+                String account = sc.nextLine();
+                user.queryUserInfo(account);
+            }
+        }
     }
 }
--- TentAndMountainWeb/src/customer/UserManagement.java
+++ TentAndMountainWeb/src/customer/UserManagement.java
@@ -1,14 +1,17 @@
 package customer;
 
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
  * @author chgwyellow
  * @create 2023-05-05 17:20
- * @客戶資料管理
- * @註冊與登入系統
+ * @用戶資料
+ * @註冊、登入和查詢資料系統
  */
 public class UserManagement {
 
@@ -16,6 +19,65 @@ public class UserManagement {
      * 建立儲存用戶資料的陣列
      */
     private ArrayList<User> userList = new ArrayList<>();
+    /**
+     * 預設註冊未成功
+     */
+    boolean succeed = false;
+
+    /**
+     * TODO 註冊方法
+     */
+    public void register() throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("請依序輸入資料\n");
+
+        System.out.println("請輸入帳號: ");
+        String account = sc.nextLine();
+        if (!(account.matches(User.Pattern_Account()))) {
+            System.out.println("Wrong account format.");
+            return;
+        }
+
+        System.out.println("請輸入密碼: ");
+        String pwd = sc.nextLine();
+        if (!(pwd.matches(User.Pattern_PassWord()))) {
+            System.out.println("Wrong password format.");
+            return;
+        }
+
+        System.out.println("請輸入姓名: ");
+        String name = sc.nextLine();
+        if (!(name.matches(User.Pattern_Name()))) {
+            System.out.println("Wrong name format.");
+            return;
+        }
+
+        System.out.println("請輸入性別(male/female): ");
+        String gender = sc.nextLine();
+        if (!(gender.equals("Male") || gender.equals("male") || gender.equals("Female") || gender.equals("female"))) {
+            System.out.println("Wrong gender.");
+            return;
+        }
+
+        System.out.println("請輸入Email: ");
+        String email = sc.nextLine();
+        if (!(email.matches(User.Pattern_Email()))) {
+            System.out.println("Wrong email format.");
+            return;
+        }
+        User user1 = new User(account, pwd, name, gender, email);
+        userList.add(user1);
+        //將用戶資料寫入文件檔中
+        PrintWriter bw = new PrintWriter(new FileWriter("./img&info/userInfo.txt"));
+        for (User user : userList) {
+            bw.println("Account: " + user.getAccount() + ",Password: " + user.getPassword() + ",Name: " + user.getName() + ",Gender: " + user.getGender() + ",Email: " + user.getEmail());
+            bw.close();
+            System.out.println(" Register is Finished. Welcome to be the part of us!");
+        }
+        succeed = true;
+
+    }
 
     /**
      * TODO 登入方法
@@ -36,46 +98,27 @@ public class UserManagement {
                 break;
             }
         }
+        System.out.println("帳號或密碼錯誤");
 
     }
 
     /**
-     * TODO 註冊方法
+     * 查詢用戶資料
      */
-    public void register() {
+    public void queryUserInfo(String account) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("請依序輸入您的資料: ");
-
-        System.out.println("請輸入帳號: ");
-        String account = sc.nextLine();
-
-        System.out.println("請輸入密碼: ");
-        String pwd = sc.nextLine();
-
-        System.out.println("請輸入姓名: ");
-        String name = sc.nextLine();
-
-        System.out.println("請輸入性別: ");
-        String gender = sc.nextLine();
-
-        System.out.println("請輸入Email: ");
-        String email = sc.nextLine();
-
-        if (!(gender.equals("Male") || gender.equals("Female"))) {
-            System.out.println("Wrong gender.");
-        } else if (!(email.matches(User.Pattern_Email()))) {
-            System.out.println("Wrong email format.");
-        } else if (!(name.matches(User.Pattern_Name()))) {
-            System.out.println("Wrong name format.");
-        } else if (!(account.matches(User.Pattern_Account()))) {
-            System.out.println("Wrong account format.");
-        } else if (!(pwd.matches(User.Pattern_PassWord()))) {
-            System.out.println("Wrong password format.");
-        } else {
-            User user1 = new User(account, pwd, name, gender, email);
-            userList.add(user1);
-            System.out.println(" Register is Finished. Welcome to be the part of us!");
+        for (User user : userList) {
+            if (user.getAccount().equals(account)) {
+                System.out.println("Account: " + user.getAccount());
+                System.out.println("Name: " + user.getName());
+                System.out.println("Gender: " + user.getGender());
+                System.out.println("Email: " + user.getEmail());
+                break;
+            }
         }
+        System.out.println("User is not found!");
+
     }
+
+
 }
