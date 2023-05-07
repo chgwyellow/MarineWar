2df92a8d68647fcf3e9a5eadb9377101618ef9f9
chgwyellow <dinnis1107@gmail.com>
2023-05-07 16:02:52 +0800

    Thread

--- JSDVN2302/src/cn/tedu/thread/ThreadDemo03.java
+++ JSDVN2302/src/cn/tedu/thread/ThreadDemo03.java
@@ -4,13 +4,14 @@ package cn.tedu.thread;
 /**
  * @author chgwyellow
  * @date 2023/5/7 15:54
- * @description 使用匿名內部類簡化
+ * @description 簡化Thread
  */
 public class ThreadDemo03 {
 
     public static void main(String[] args) {
 
-        Thread t1 = new Thread() { //匿名內部類
+        //匿名內部類
+        Thread t1 = new Thread() {
             @Override
             public void run() {
                 for (int i = 0; i < 1000; i++) {
@@ -18,8 +19,28 @@ public class ThreadDemo03 {
                 }
             }
         };
-        t1.start();
 
+        //實作Runnable
+        Runnable r1 = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    System.out.println("項洗哩?");
+                }
+            }
+        };
+        Thread t2 = new Thread(r1);
+
+        //透過Lambda表達式簡化
+        Runnable r2 = () -> {
+            for (int i = 0; i < 1000; i++) {
+                System.out.println("項洗哩?");
+            }
+        };
+        Thread t3 = new Thread(r2);
+
+        t1.start();
+        t2.start();
     }
 
 }
